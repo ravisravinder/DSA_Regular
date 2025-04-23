ac84934bc070379df5e84f3787f86c5f51fd37ae
ravisravinder <115361406+ravisravinder@users.noreply.github.com>
2025-04-23 09:05:13 +0530

    Update Basics.java

--- Basics.java
+++ Basics.java
@@ -20,4 +20,56 @@ public class MakeArrayNonDecreasing {
 }
 
 -----------------------------------------------------------------------------
-  
+  package leetcode;
+
+import java.util.Arrays;
+
+public class LongestIncreasingSubArray {
+    public static void main(String[] args) {
+        int nums[] = {1, 3, 5, 4, 7};
+        longestIncreasingSubArray(nums);
+        longestIncreasingSubArraySize(nums);
+    }
+
+    private static int[] longestIncreasingSubArray(int[] nums) {
+        if (nums.length == 0)
+            return new int[]{};
+        int startIndex = 0;
+        int maxLength = 1;
+
+        int curStartIndex = 0;
+        int curLength = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                curLength++;
+               if(curLength>maxLength){
+                   maxLength= curLength;
+                   startIndex=curStartIndex;
+               }
+            } else {
+                curLength = 1;
+                curStartIndex = i;
+            }
+        }
+        return Arrays.copyOfRange(nums,startIndex, startIndex+maxLength);
+    }
+
+    private static int longestIncreasingSubArraySize(int[] nums) {
+        if (nums.length == 0)
+            return 0;
+        int maxLength = 1;
+        int curLe = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                curLe++;
+                maxLength = Math.max(curLe, maxLength);
+            } else {
+                curLe = 1;
+            }
+        }
+        return maxLength;
+    }
+}
+
+-------------------------------------------
+    
